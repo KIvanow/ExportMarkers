8300f333fdf7f0c000cd01bfc7affe6b0dceba21
KIvanow <k.ivanow@gmail.com>
2014-07-22 15:53:43 +0300

    fixed new line issue and wrapped the entire sprite in define return statements

--- export_markers.py
+++ export_markers.py
@@ -12,11 +12,13 @@ class ExportMarkersCommand(sublime_plugin.TextCommand):
 
 		sampleRate 	= 44100
 		spritemap = ''
+		newLine = '\n'
 		tab = '\t'
 
-		spriteMap += "sound: { " + os.linesep + "\t resources: [ '" + str( self.view.file_name().split(os.sep)[-1:][0] ) + "', '" + str( self.view.file_name().split(os.sep)[-1:][0][ 0 : -3] ) + 'ogg' + "' ], " + os.linesep + "\tspritemap:{" + os.linesep
-		spriteMap += "\t\t'empty': { " + os.linesep + "\t\t\t'start' : 0, " + os.linesep + "\t\t\t'end': 0.5, " + os.linesep + "\t\t\t'loop': false " + os.linesep + "\t\t}, " + os.linesep
-		spriteMap += "\t\t'emptyLoop': { " + os.linesep + "\t\t\t'start' : 0, " + os.linesep + "\t\t\t'end': 0.5, " + os.linesep + "\t\t\t'loop': true " + os.linesep + "\t\t}, " + os.linesep
+		spriteMap = "define(function(){" + newLine + "\treturn {" + newLine
+		spriteMap += "\t\tresources: [ '" + str( self.view.file_name().split(os.sep)[-1:][0] ) + "', '" + str( self.view.file_name().split(os.sep)[-1:][0][ 0 : -3] ) + 'ogg' + "' ], " + newLine + "\t\tspritemap:{" + newLine
+		spriteMap += "\t\t\t'empty': { " + newLine + "\t\t\t\t'start' : 0, " + newLine + "\t\t\t\t'end': 0.5, " + newLine + "\t\t\t\t'loop': false " + newLine + "\t\t\t}, " + newLine
+		spriteMap += "\t\t\t'emptyLoop': { " + newLine + "\t\t\t\t'start' : 0, " + newLine + "\t\t\t\t'end': 0.5, " + newLine + "\t\t\t\t'loop': true " + newLine + "\t\t\t}, "
 
 		for i in range( 0, len( words ) ):
 			if 'xmpDM:frameRate' in words[ i ]:
@@ -27,9 +29,9 @@ class ExportMarkersCommand(sublime_plugin.TextCommand):
 				start = int( words[i-2].split('"')[1] ) / sampleRate
 				end = start + math.floor( float( words[i-1].split('"')[1] ) / sampleRate * 1000 ) / 1000
 				loop = 'true' if 'loop' in words[i].split('"')[1].lower() else 'false'
-				spriteMap += '\t\t"' + words[i].split('"')[1] + '": { ' + os.linesep + '\t\t\t"start": ' + str( start ) + ', ' + os.linesep + '\t\t\t"end": ' + str( end ) + ', ' + os.linesep + '\t\t\t"loop": ' + loop + os.linesep + '\t\t},' + os.linesep
+				spriteMap += newLine + '\t\t\t"' + words[i].split('"')[1] + '": { ' + newLine + '\t\t\t\t"start": ' + str( start ) + ', ' + newLine + '\t\t\t\t"end": ' + str( end ) + ', ' + newLine + '\t\t\t\t"loop": ' + loop + newLine + '\t\t\t},'
 
-		spriteMap += '\t} ' + os.linesep + '}'
+		spriteMap = spriteMap[ 0: -1 ] + newLine + '\t\t} ' + newLine + '\t} ' + newLine + '})'
 
 		spriteFile = sublime.Window.new_file(self.view.window())
 		spriteFile.insert(edit, 0, spriteMap)
